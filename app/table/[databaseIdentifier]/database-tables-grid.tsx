17cdcb98de7eb51ff61ebb2f381d9a1fbe89abad
Cameron Bristol <cbristol28@yahoo.com>
2024-04-29 04:24:07 -0500

    database-tables-grid creation

--- app/table/[databaseIdentifier]/database-tables-grid.tsx
+++ app/table/[databaseIdentifier]/database-tables-grid.tsx
@@ -4,12 +4,14 @@ import { AgGridReact } from "ag-grid-react"; // AG Grid Component
 import "ag-grid-community/styles/ag-grid.css"; // Mandatory CSS required by the grid
 import "ag-grid-community/styles/ag-theme-balham.css"; // Optional Theme applied to the grid
 import { DbSchemaResponseType } from "./schemas";
+import { forwardRef } from "react";
 
-export function TableSchemasDisplay({
-  schemas,
-}: {
-  schemas: DbSchemaResponseType[];
-}) {
+export const DatabaseTablesGrid = forwardRef<
+  AgGridReact,
+  {
+    tables: DbSchemaResponseType[];
+  }
+>(({ tables }, ref) => {
   return (
     // wrapping container with theme & size
     <div
@@ -17,8 +19,9 @@ export function TableSchemasDisplay({
       style={{ height: 200 }} // the grid will fill the size of the parent container
     >
       <AgGridReact
-        rowData={schemas.map((schema) => ({
-          tableName: schema.tablename,
+        ref={ref}
+        rowData={tables.map((table) => ({
+          tableName: table.tablename,
         }))}
         columnDefs={[
           {
@@ -29,10 +32,9 @@ export function TableSchemasDisplay({
           },
         ]}
         rowSelection="multiple"
-        onSelectionChanged={(currentState) => {
-          console.log(currentState.api.getSelectedRows());
-        }}
       />
     </div>
   );
-}
+});
+
+DatabaseTablesGrid.displayName = "DatabaseTablesDisplay";
