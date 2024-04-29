ee3ead11d6f0de667bb7629d84251e11e422d04f
Mhanna112-code <Marc.Hanna-1@ou.edu>
2024-04-29 01:13:16 -0500

    made migration button disabled when user begins migration

--- app/table/[databaseName]/components/StartMigrationButton.tsx
+++ app/table/[databaseName]/components/StartMigrationButton.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { useState } from 'react';
 import { runDatabaseMigration } from "../actions/migrate-database/run-migration";
 
 export function StartMigrationButton({
@@ -10,17 +11,26 @@ export function StartMigrationButton({
   databasePassword: string;
   tableNames: [string, ...string[]];
 }) {
+  // State to control the disabled status of the button
+  const [isDisabled, setIsDisabled] = useState(false);
+
   return (
     <button
-      className="px-4 py-2 dark:bg-slate-500 rounded-md"
+      className={`px-4 py-2 rounded-md ${isDisabled ? 'bg-gray-400 cursor-not-allowed' : 'bg-white'}`}
+      disabled={isDisabled} // Button disabled based on state
       onClick={async () => {
-        await runDatabaseMigration({
-          rdsDatabaseInfo: {
-            identifier: databaseIdentifier,
-            password: databasePassword,
-          },
-          tableNames,
-        });
+        setIsDisabled(true); // Disable button when clicked
+        try {
+          await runDatabaseMigration({
+            rdsDatabaseInfo: {
+              identifier: databaseIdentifier,
+              password: databasePassword,
+            },
+            tableNames,
+          });
+        } finally {
+          setIsDisabled(false); // Re-enable button after operation completes
+        }
       }}
     >
       Begin Migration
